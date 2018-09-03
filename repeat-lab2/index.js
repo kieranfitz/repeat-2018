f9337e0eceb2029c938bb88b446f678f54c29b73
Kieran Fitzgerald <pte_fitz@hotmail.com>
2018-09-03 13:00:15 +0100

    added routing for add,update,delete contact

--- repeat-lab2/index.js
+++ repeat-lab2/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import gamesRouter from './api/games';
 import bodyParser from 'body-parser';
+import postsRouter from './api/posts';
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 
 app.use('/api/games', gamesRouter);
-app.use(express.static('public'));
+app.use('/api/posts', postsRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
